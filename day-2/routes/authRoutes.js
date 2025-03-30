e005a539f1f43bff6ce9cdd3828c44f0711465b5
ratatatcode <pogisijamesmagnaye@gmail.com>
2025-03-30 12:06:59 +0800

    Update mentoring -> day 2: Firebase Auth with Postman

---
+++ day-2/routes/authRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+const { signup, login } = require("../controllers/authController");
+
+router.get('/', (req, res) => {
+    res.render("landingpage.ejs");
+});
+
+router.post('/api/auth/signup', signup);
+router.post('/api/auth/login', login);
+
+module.exports = router;
--- day-2/server.js
+++ day-2/server.js
@@ -1,16 +1,15 @@
 const express = require('express');
 const app = express();
-const path = require(`path`);
+const path = require('path');
 
+const auth = require('./routes/authRoutes');
+
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/static', express.static(path.join(__dirname, 'public')));
-
 app.set('view engine', 'ejs');
 
-// localhost:3000/
-app.get('/', (req, res) => {
-    res.render("landingpage.ejs");
-});
+app.use("/", auth);
 
 const PORT = 3000;
 app.listen(PORT, () => {
